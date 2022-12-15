3f9f7fafc07e867380c41a65929517111eef377c
Satoshi Matsushima <work.satoshi1993@gmail.com>
2022-12-15 22:39:43 +0900

    Markuped and styled Facility.tsx

--- src/components/Facility.tsx
+++ src/components/Facility.tsx
@@ -1,6 +1,25 @@
+import { Container, Paper, TextField } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import React from 'react';
 
+const RootContainer = styled(Container)(({ theme }) => ({
+  '& .MuiTextField-root': {
+    margin: theme.spacing(1),
+  },
+}));
+
+const PpaperForm = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(1),
+}));
+
 const Facility: React.FC = () => {
-  return <div>Facility</div>;
+  return (
+    <RootContainer maxWidth="sm">
+      <PpaperForm>
+        <TextField label="設備名" fullWidth />
+        <TextField label="詳細" fullWidth multiline />
+      </PpaperForm>
+    </RootContainer>
+  );
 };
 export default Facility;
