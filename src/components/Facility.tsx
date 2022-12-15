f9b75da13b664175e8be639829553998cb68cd06
Satoshi Matsushima <work.satoshi1993@gmail.com>
2022-12-15 23:14:32 +0900

    added dayjs  and markuped input area

--- src/components/Facility.tsx
+++ src/components/Facility.tsx
@@ -1,5 +1,13 @@
-import { Container, Paper, TextField } from '@mui/material';
+import {
+  Avatar,
+  Chip,
+  Container,
+  InputLabel,
+  Paper,
+  TextField,
+} from '@mui/material';
 import { styled } from '@mui/material/styles';
+import dayjs from 'dayjs';
 import React from 'react';
 
 const RootContainer = styled(Container)(({ theme }) => ({
@@ -18,6 +26,25 @@ const Facility: React.FC = () => {
       <PpaperForm>
         <TextField label="設備名" fullWidth />
         <TextField label="詳細" fullWidth multiline />
+        <InputLabel shrink>登録者</InputLabel>
+        <p>
+          <Chip
+            label="登録者"
+            avatar={
+              <Avatar src="https://avatars.githubusercontent.com/u/58695418?v=4" />
+            }
+          />
+          {dayjs(new Date()).format('YYYY-MM-DD HH:mm')}
+        </p>
+        <p>
+          <Chip
+            label="更新者"
+            avatar={
+              <Avatar src="https://avatars.githubusercontent.com/u/58695418?v=4" />
+            }
+          />
+          {dayjs(new Date()).format('YYYY-MM-DD HH:mm')}
+        </p>
       </PpaperForm>
     </RootContainer>
   );
